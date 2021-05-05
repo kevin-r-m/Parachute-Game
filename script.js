8c7dc11920c126a6925cc7c8e367c0e941778566
kevin-m <kmclaughlinbtb@gmail.com>
2021-05-04 19:33:34 -0500

    Building up comparison logic javascript comparison logic. Some HTML & CSS elements added to support js

--- script.js
+++ script.js
@@ -2,24 +2,55 @@ console.log('this works')
 
 //capture form results
 
-const wordSubmit = document.querySelector('.wordSubmit')
-const word = document.querySelector('#charSubmit')
+const submitBttn = document.querySelector('.wordSubmit')
+const word = document.querySelector('#wordField')
 
 
-wordSubmit.addEventListener('click', function(event){
+submitBttn.addEventListener('click', function(event){
     event.preventDefault();
     // submitLogic();
     // grabbing the word entered and putting it into an array
-    const charArray = Array.from(word.value)
+    var charArray = Array.from(word.value)
     console.log(charArray)
     // Use static value to check for certain characters in new array of characters
     // checkLogic(charArray)
+    //Push charArray into html so it can be accessed by the user guesses
     charArray.forEach(char => {
-        if(char === 'l'){
-            console.log('match')
-        }
+        let li = document.createElement('li')
+        li.innerText = char
+        //Adding class so it can access later in guess character submission (lines 41 - 44)
+        li.classList = 'guessChars'
+        li.setAttribute('data-figure', char)
+        let ul = document.querySelector('.guessWord')
+        ul.appendChild(li)
     })
 })
+
+
+
+//User submission for letter guessed
+
+//Need to capture the user submisson for a guess letter
+const charGuess = document.querySelector('#charField')
+const charSubmit = document.querySelector('.charSubmit')
+//Need to grab the submitted letter after user submits
+charSubmit.addEventListener('click', function(event){
+    event.preventDefault();
+    const char = charGuess.value
+    //Need to access HTML li elements created with initial user submission lines 18 - 23
+    let guessArray = document.querySelectorAll('.guessChars')
+    guessArray.forEach(guess => {
+        if (guess.innerText === char){
+            console.log('match')
+
+        }
+
+    })
+    console.log(char)
+})
+//Need to use the submitted letter to compare it against the array of characters for the word
+
+
 
 //Function for submission logic
     // function submitLogic(){
