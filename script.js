f1890bc067900166969097853924e6d54d4b3c28
kevin-m <kmclaughlinbtb@gmail.com>
2021-05-08 11:15:17 -0500

    Added more css structure for MVP and add end game logic framework

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-console.log('this works')
 
 //capture form results
 
 const submitBttn = document.querySelector('.wordSubmit')
 const word = document.querySelector('#wordField')
+const startGame = document.querySelector('.gameStart')
 
 
 submitBttn.addEventListener('click', function(event){
@@ -12,6 +12,12 @@ submitBttn.addEventListener('click', function(event){
     // grabbing the word entered and putting it into an array
     var charArray = Array.from(word.value)
     console.log(charArray)
+    let spaceDetector = (charArray.includes(' '))
+    if(spaceDetector === true){
+        console.log('space detected')
+        return
+    }
+    
     // Use static value to check for certain characters in new array of characters
     // checkLogic(charArray)
     //Push charArray into html so it can be accessed by the user guesses
@@ -24,6 +30,10 @@ submitBttn.addEventListener('click', function(event){
         let ul = document.querySelector('.guessWord')
         ul.appendChild(li)
     })
+    setTimeout(function(){
+        startGame.style.opacity = 0
+        startGame.style.pointerEvents = 'none'
+    }, 1000)
 })
 
 
@@ -40,10 +50,10 @@ charSubmit.addEventListener('click', function(event){
     //Need to access HTML li elements created with initial user submission lines 18 - 23
     let guessArray = document.querySelectorAll('.guessChars')
     //Need to use the submitted letter to compare it against the array of characters for the word submitted
-    
+    charGuess.value = ''
     var correct = false
     guessArray.forEach(guess => {
-        if (guess.innerText === char){
+        if (guess.innerText == char){
             console.log('match')
             correct = true
             //Need to access available letters style on match
@@ -55,7 +65,6 @@ charSubmit.addEventListener('click', function(event){
         updateGuesses()
         endGame()
     }
-    console.log(char)
 })
 
 let guesses = 5
@@ -70,6 +79,10 @@ function endGame() {
     if (guesses <= 0){
         console.log('Its over man')
     }
+}
+
+function winGame (){
+    console.log(guessArray)
 }
 
 //Function for submission logic
